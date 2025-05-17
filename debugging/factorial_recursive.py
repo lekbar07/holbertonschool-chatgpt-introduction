1b67183f98ed59e17cae77d0973a4a7638e0e9cc
lekba07 <lekbar@bamis.mr>
2025-05-17 14:01:56 +0000

    factorial_recursive.py

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -2,10 +2,23 @@
 import sys
 
 def factorial(n):
+    if n < 0:
+        raise ValueError("La factorielle n'est pas définie pour les entiers négatifs.")
     if n == 0:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n - 1)
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ./factorial.py <nombre_entier>")
+        sys.exit(1)
+
+    try:
+        number = int(sys.argv[1])
+        f = factorial(number)
+        print(f)
+    except ValueError as e:
+        print(f"Erreur : {e}")
+        sys.exit(1)
+
